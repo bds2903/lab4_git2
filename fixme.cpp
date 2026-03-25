12a5c35a4acae32c90a1fb6f7fa9ae25b627088e
Dima <bds2903@gmail.com>
2026-03-25 09:54:16 +0300

    Добавил файл

---
+++ fixme.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int a = 5
+    int b = 10;
+
+    if (a < b)
+        std::cout << "A меньше B" << std::endl
+    else
+        std::cout << "B меньше A" << std::endl;
+
+    return 0;
+}
